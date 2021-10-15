556e69887c2ece46484ec4eac75a9b45182c3492
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-10-15 15:41:15 -0500

    Se renombra la clase de saludar.js por saludar.tsx

--- src/domain/saludar.js
+++
@@ -1,4 +0,0 @@
-
-export const concatenarSaludo = (valorCampo) => {
-    return `Hola ${valorCampo.trim()}`
-};
---
+++ src/domain/saludar.tsx
@@ -0,0 +1,4 @@
+
+export function concatenarSaludo (valorCampo: string): string {
+    return `Hola ${valorCampo.trim()}`
+};
